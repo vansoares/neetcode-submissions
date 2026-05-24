75f6ba0cfe101eb3597a0804a08d358322367aef
Vanessa Soares <vanessa.ssoares.vs@gmail.com>
2026-05-24 19:31:41 -0300

    Add: find-target-in-rotated-sorted-array - submission-1

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-1.go
@@ -0,0 +1,31 @@
+
+
+func search(nums []int, target int) int {
+	left := 0
+	right := len(nums)-1
+	
+	for left <= right {
+		
+		mid := (left + right)/2
+		if nums[mid] == target {
+			return mid
+		}
+
+		if nums[left] <= nums[mid] {
+			if nums[left] <= target && nums[mid] > target {
+				right = mid -1
+			} else {
+				left = mid + 1
+			}
+		} else {
+			if nums[mid] < target && nums[right] >= target {
+				left = mid + 1 
+			} else {
+				right = mid - 1
+			}
+		}
+	}
+
+	return -1
+
+}
